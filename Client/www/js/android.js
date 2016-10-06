a405bca890924acdc4816da2d2ed977dec5e7d92
Mikhail Gribkov <gmn.smichrissoft@gmail.com>
2016-10-06 17:52:20 +0300

    Очистил клиент от остатков старого приложения

--- Client/www/js/android.js
+++ Client/www/js/android.js
@@ -1,8 +1,19 @@
+var serveraddr = "http://upw.gtwenty.com"; //Адрес сервера в формате http://example.com:8888, закрывающий слэш НЕ НУЖЕН!
+
+function toggleFullScreenModal(element){
+  $(element).slideToggle();
+}
+//Check if email is valid
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
 //Check if user is logged in
 function init(){
   if (window.localStorage.getItem("islogged") !== "true"){
     $('#header').css('display', 'none');
     $('#content').load('templates/android/login/login.html');
+    $('select').material_select();
   } else {
     $(".button-collapse").sideNav({closeOnClick: true});
     var data = JSON.stringify({'id': localStorage.getItem("uid")});
@@ -10,58 +21,25 @@ function init(){
     method: "POST",
     dataType: "JSON",
     crossDomain: true,
-    url: "http://localhost:8888/account/contactData",
-    data: {'data': data},
-
+    url: serveraddr+"/account/getAccountData",
+    data: data
   }).done(function(msg){
-    
-    if(msg.message.photo == ''){
-      $('#sidenav-avatar').attr('src', '/img/default-avatar.jpg');
-    } else {
-        $('#sidenav-avatar').attr('src', msg.message.photo);
-    }
-    $('#user-name').text(msg.message.firstName+" "+msg.message.lastName);
-    $('#user-position').text(msg.message.position);
-
+    $('#user-nickname').text(msg.nickname);
+    $('#user-login').text(msg.login);
   });
   }
 }
-//Open registration dialog
-function openRegisterDialog(){
-  $('#header').css('display', 'none');
-  $('#content').load('templates/android/register/stepone.html');
-  /*
-  * ToDo: Сделать ловилку изменений формы, если поле пустое, то сделать disabled кнопку далее
-  $('#register-form > div.input-field > input').keypress(function(){
-    if($(this).val() == ''){
-      $('#bottom-nav-row > a').removeClass('black-text');
-      $('#bottom-nav-row > a').addClass('grey-text');
-    }
-  });
-  */
-  if(localStorage.getItem("register-email")){
-    $('#register-email').attr('value', localStorage.getItem("register-email"));
-    console.log($('#register-email').val());
-  }
-  if(localStorage.getItem("register-pass")){
-    $('#register-pass').val(localStorage.getItem("register-pass"));
-    $('#register-repass').val(localStorage.getItem("register-pass"));
-    console.log($('#register-pass').val());
-  }
-}
-
-
 //Handle the login form
 function login(){
-  var email = $('#login-email').val();
+  var login = $('#login-login').val();
   var pass = $('#login-pass').val();
-  var data = JSON.stringify({'email': email, 'password': pass});
+  var data = JSON.stringify({'login': login, 'password': pass});
   $.ajax({
   method: "POST",
   dataType: "JSON",
   crossDomain: true,
   url: "http://localhost:8888/auth/auth",
-  data: {'data': data}
+  data: data
   }).done(function(msg){
     if(msg.message == 'Success'){
       window.localStorage.setItem("islogged", "true");
@@ -72,25 +50,32 @@ function login(){
     }
   });
 }
-
-
+function logout(){
+  window.localStorage.clear();
+  location.href = "index-and.html";
+}
 $('#login-email').change(function(){
   $('#login-result').html('');
 });
-
-
 $('#login-pass').change(function(){
   $('#login-result').html('');
 });
 
-//Check if email is valid
-function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
-}
 
-//Check the first step of registration and proceed to step 2
-function steptwo(){
+//Грузим регистрацию
+function registerStepOne(){
+  $('#header').css('display', 'none');
+  $('#content').load('templates/android/register/stepone.html');
+  if(localStorage.getItem("register-email")){
+    $('#register-email').attr('value', localStorage.getItem("register-email"));
+  }
+  if(localStorage.getItem("register-pass")){
+    $('#register-pass').val(localStorage.getItem("register-pass"));
+    $('#register-repass').val(localStorage.getItem("register-pass"));
+  }
+}
+//Проверяем 1й шаг
+function registerStepTwo(){
   console.log('checking form');
   var email = $('#register-email').val();
   var pass = $('#register-pass').val();
@@ -117,85 +102,6 @@ function steptwo(){
     //do nothing
   }
 }
-
-
-
 function passRecovery(){
   //ToDo: Написать функцию
 }
-
-function openNotificationsList(){
-  init();
-  $('#content').load('templates/android/notifications/notifications.html');
-  $('select').material_select();
-}
-
-//opens notification add modal
-function openNotificationsAdd(){
-  $('#modal-add-notification').openModal();
-  $('#upsert-btn-ok').click(function(){
-    addNotification();
-    openNotificationsList();
-  });
-  $('#noti-modal-header').text('New notification');
-}
-
-function openNotificationsEdit(id){
-  $('#modal-add-notification').openModal();
-  $('#upsert-btn-ok').click(function(){
-    saveNotification(id);
-    openNotificationsList();
-  });
-$('#noti-modal-header').text('Edit notification');
-}
-function addNotification(){
-  //ToDo: Написать функцию
-}
-function saveNotification(id){
-  //ToDo: И эту тоже
-}
-function openSettings(){
-  $('#content').load('templates/android/settings/settings.html');
-}
-function logout(){
-  window.localStorage.clear();
-  location.href = "index-and.html";
-}
-function toggleFullScreenModal(element){
-  $(element).slideToggle();
-}
-function toggleNotificationsAdd2(){
-  toggleFullScreenModal('#test-modal');
-}
-
-function openeditprofile(){
-  toggleFullScreenModal($('#editprofile-modal'));
-  var data = JSON.stringify({'id': localStorage.getItem("uid")});
-  $.ajax({
-  method: "POST",
-  dataType: "JSON",
-  crossDomain: true,
-  url: "http://localhost:8888/account/contactData",
-  data: {'data': data},
-  }).done(function(msg){
-    $('#profile-fname').val(msg.message.firstName);
-    $('#profile-mname').val(msg.message.middleName);
-    $('#profile-lname').val(msg.message.lastName);
-    $('#profile-position').val(msg.message.position);
-    if(msg.message.photo != ""){
-      $('#avatar-img').attr('src', msg.message.photo);
-    } else {
-      $('#avatar-img').attr('src', '/img/default-avatar.jpg');
-    }
-  });
-}
-function shownewimage(){
-  $('#avatar-pic-upload').change();
-}
-$('#avatar-pic-upload').change(function(){
-  $(this).base64img({
-    url: e.target.files[0],
-    result: '#hiddenphotodata'
-  });
-  $('#avatar-img').attr('src', $('#hiddenphotodata').val());
-});
